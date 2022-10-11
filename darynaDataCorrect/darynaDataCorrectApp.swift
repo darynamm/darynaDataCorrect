8eed670c1997dedaaf8e635618095e30008647a3
darynamm <darynamanuilenko@gmail.com>
2022-10-11 07:43:06 -0600

    made repo and git ignore

---
+++ darynaDataCorrect/darynaDataCorrectApp.swift
@@ -0,0 +1,17 @@
+//
+//  darynaDataCorrectApp.swift
+//  darynaDataCorrect
+//
+//  Created by Manuilenko, Daryna on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct darynaDataCorrectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
