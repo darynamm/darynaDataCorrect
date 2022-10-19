849db946c83fc43767f8551788cae23d7e86b0c7
darynamm <darynamanuilenko@gmail.com>
2022-10-19 09:41:19 -0600

    made the struct and converted it to a filw

---
+++ darynaDataCorrect/Model/File.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  darynaDataCorrect
+//
+//  Created by Manuilenko, Daryna on 10/19/22.
+//
+
+import Foundation
+
+
+struct GradeAcess
+{
+    var Year : Int
+    var state : String
+    var math : Int
+    var takers : Int
+    var verbal : Int
+    var averageGpa : Int
+    var academicYears : Int
+    var AcademicGPA : Int
+    var englishGPA : Int
+    var foreignlanguageGPA : Int
+    var foreighnlanguageYears : Int
+    var mathGPA : Int
+    var mathYears : Int
+    var scienceGPA : Int
+
+}
