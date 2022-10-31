e65aa6568e1656f412cfb1fb98ffb54274aaf434
darynamm <darynamanuilenko@gmail.com>
2022-10-31 08:11:43 -0600

    made the bucket list item store the buckets

--- darynaDataCorrect/Model/BucketStore.swift
+++ darynaDataCorrect/Model/BucketStore.swift
@@ -23,6 +23,19 @@ class BucketStore : ObservableObject
     }
     init(buckets : [BucketListItem])
     {
+        
+        if let storedOnDevice = UserDefaults.standard.data(forKey: "Saved Buckets")
+        {
+            let decoder = JSONDecoder()
+            if let savedBuckets = try? decoder.decode([BucketListItem].self, from: storedOnDevice)
+            {
+                if (savedBuckets.count != 0)
+                {
+                    self.buckets = savedBuckets
+                    return //early return since we r loading data from device
+                }
+            }
+        }
         self.buckets = buckets 
     }
 }
