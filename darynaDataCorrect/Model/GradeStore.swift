6832a974b70db1a423f449ccd5d82a218de0cb0f
darynamm <darynamanuilenko@gmail.com>
2022-10-31 08:39:24 -0600

    made a didset for the grade store

--- darynaDataCorrect/Model/GradeStore.swift
+++ darynaDataCorrect/Model/GradeStore.swift
@@ -12,8 +12,32 @@ import Combine
 class GradeStore : ObservableObject
 {
     @Published var grades : [GradeAccess]
+    {
+        didSet
+        {
+            let encoder = JSONEncoder()
+            if let encodedGrades = try? encoder.encode(grades)
+            {
+                UserDefaults.standard.set(encodedGrades, forKey: "Saved Grades")
+            }
+        }
+    }
+    
     init(grades : [GradeAccess])
     {
+        if let storedOnDevice = UserDefaults.standard.data(forKey: "Saved grades")
+        {
+            let decoder = JSONDecoder()
+            if let savedGrades = try? decoder.decode([GradeAccess].self, from: storedOnDevice)
+            {
+                if (savedGrades.count != 0)
+                {
+                    self.grades = savedGrades
+                    return
+                }
+            }
+            
+        }
         self.grades = grades
     }
 }
